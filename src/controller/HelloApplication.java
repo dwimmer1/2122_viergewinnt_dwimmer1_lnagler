af753703ac7d6b9c00c419ae161b551ee3a84da4
lnagler1 <lnagler1@htl-steyr.ac.at>
2022-02-13 19:27:06 +0100

    GUI Gamefield begonnen

--- src/controller/HelloApplication.java
+++ src/controller/HelloApplication.java
@@ -3,16 +3,26 @@ package controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+
+import static controller.GameController.play;
 
 public class HelloApplication extends Application {
-    @Override
+    HelloController h = new HelloController();
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(h.gridPane);
+        h.initialize();
+
+        Scene scene = new Scene(root, 600, 600);
+
+        stage.setTitle("Pyramid");
         stage.setScene(scene);
         stage.show();
     }
--- src/controller/HelloController.java
+++ src/controller/HelloController.java
@@ -1,17 +1,53 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+
+import static javafx.application.Application.launch;
 
 public class HelloController {
-    public Label jack;
-    @FXML
-    private Label welcomeText;
+    GridPane gridPane = new GridPane();
+
+
+    public Node getNodeFromGridPane(GridPane gridPane, int col, int row) {
+        for (Node node : gridPane.getChildren()) {
+            int nodeCol = GridPane.getColumnIndex(node);
+            int rowCol = GridPane.getRowIndex(node);
+            if (nodeCol == col && rowCol == row) {
+                return node;
+            }
+        }
+        return null;
+    }
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+        System.out.println("start");
 
-        jack.setText("Wimma do issa");
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                System.out.println("\nZeile" + i + "Spalte" + j);
+                Circle circle = new Circle(40, Color.WHITE);
+                Pane pane = new Pane();
+                pane.setStyle("-fx-background-color: blue");
+                gridPane.add(pane,j,i);
+                gridPane.add(circle, j, i);
+            }
+
+
+        }
     }
 }
+
