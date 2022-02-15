add53ff2ec900181aff6f15e561afe7d80dbc796
lnagler1 <lnagler1@htl-steyr.ac.at>
2022-02-15 18:00:34 +0100

    GUI

--- src/controller/HelloApplication.java
+++ src/controller/HelloApplication.java
@@ -26,6 +26,7 @@ public class HelloApplication extends Application {
         }
     }
 
+
     public static void main(String[] args) {
         launch();
     }
--- src/controller/HelloController.java
+++ src/controller/HelloController.java
@@ -2,54 +2,66 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.Button;
 
 
-import javax.xml.transform.Source;
-import java.awt.*;
-import java.io.IOException;
-import java.util.Objects;
+import java.util.EventObject;
 
 import static javafx.application.Application.launch;
 
 public class HelloController {
     public Button start;
     GridPane gridPane = new GridPane();
-    AnchorPane anchor = new AnchorPane();
     Button[] button = new Button[7];
     VBox[] vBox = new VBox[7];
     Circle[] circle = new Circle[42];
+    int row1 = 7;
+
+
+    EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent e) {
+            Node source = (Node)e.getSource();
+            System.out.println(source);
+            int row = GridPane.getRowIndex(source);
+            int col = GridPane.getColumnIndex(source);
+            Circle circle1 = new Circle(40, Color.YELLOW);
+
+            gridPane.add(circle1, col, row1);
+            for (Node node : gridPane.getChildren()) {
+                int nodeCol = GridPane.getColumnIndex(node);
+                int rowCol = GridPane.getRowIndex(node);
+            }
+
+
+        }
+    };
+
 
     @FXML
     public void initialize() {
         System.out.println("start");
         gridPane.setGridLinesVisible(false);
 
-        //gridPane.setOnMouseClicked(clickGrid();
+
+
         int k = 0;
         for (int i = 1; i < 7; i++) {
             for (int j = 0; j < 7; j++) {
-                if (i == 1){
+                if (i == 1) {
                     button[k] = new Button("Click");
                     vBox[k] = new VBox();
                     vBox[k].setAlignment(Pos.CENTER);
-                    button[k].setPrefSize(70,30);
+                    button[k].setPrefSize(70, 30);
                     vBox[k].getChildren().add(button[k]);
                     gridPane.add(vBox[k], j, 0);
                     k++;
@@ -60,18 +72,23 @@ public class HelloController {
                 pane.setStyle("-fx-background-color: blue");
                 gridPane.add(pane, j, i);
                 gridPane.add(circle[k], j, i);
+                circle[k].addEventHandler(MouseEvent.MOUSE_CLICKED, eventHandler);
 
             }
             System.out.println("Hallo");
         }
+
+
     }
 
-    public void MouseEvent(){
-        for (Node source: gridPane.getChildren()){
-            int col = GridPane.getColumnIndex(source);
-            int row = GridPane.getRowIndex(source);
-            System.out.println(col + row);
-        }
+
+
+
+        private void mouseClicked(MouseEvent e) {
+        Node source = (Node)e.getSource() ;
+        Integer colIndex = GridPane.getColumnIndex(source);
+        Integer rowIndex = GridPane.getRowIndex(source);
+        System.out.printf("Mouse entered cell [%d, %d]%n", colIndex, rowIndex);
     }
 
     //Wenn startButton gedrückt wird, wird das Gamefield-Fenster geöffnet.
